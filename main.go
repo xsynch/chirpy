09f8eb7b55ad6ae69588da2ffc656d0b07a69195
xsynch <jdstrickland@gmail.com>
2024-11-27 23:22:05 -0500

    add middleware to collect number of hits and headers to /app. Also added /metrics,/reset,/healthz endpoints

--- main.go
+++ main.go
@@ -1,23 +1,86 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func handleHealth(w http.ResponseWriter, r *http.Request){
+	w.Header().Add("Content-Type","text/plain; charset=utf-8")	
+	
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits = atomic.Int32{}
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(http.StatusText(http.StatusOK)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func (cfg *apiConfig)getMetrics(w http.ResponseWriter, r *http.Request){
+	currentHits := cfg.fileserverHits.Load()
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte(fmt.Sprintf("Hits: %d", currentHits)))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+		
+		
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+			cfg.fileserverHits.Add(1)
+			next.ServeHTTP(w,r)
+		}) 
+}
+
+func (cfg *apiConfig) addHeaders(next http.Handler) http.Handler{
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
+		w.Header().Add("Cache-Control","no-cache")
+		next.ServeHTTP(w,r)
+	})
+}
 
 func main(){
+
+rootDir := "."
+httpPort := 8080
+
+apiConfig := apiConfig{fileserverHits: atomic.Int32{}}
+
 mux := http.NewServeMux()
 
 server := &http.Server{
 	Addr: ":8080",
 	Handler: mux,
 }
-mux.Handle("/", http.FileServer(http.Dir(".")))
-mux.Handle("/assets", http.FileServer(http.Dir("assets")))
+
+fs := http.FileServer(http.Dir(rootDir))
+mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app",apiConfig.addHeaders(fs))))
+
+mux.HandleFunc("/healthz", handleHealth)
+mux.HandleFunc("/metrics",apiConfig.getMetrics)
+mux.HandleFunc("/reset", apiConfig.reset)
 
 
-log.Printf("Server files from '.' on port: 8080")
+log.Printf("Server files from %s on port: %d",rootDir, httpPort)
 log.Fatal(server.ListenAndServe())
 
 
